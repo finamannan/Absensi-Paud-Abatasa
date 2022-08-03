a27c5fa767ea3d9438dc055cc9cf89aa37c13561
Ameng <107167038+cameliasyarifa@users.noreply.github.com>
2022-08-03 14:10:11 +0700

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,14 @@
+<?php 
+    /**
+     * 
+     *  Absensi Guru PAUD ABATASA
+     *  codinger-mini.blogpsot.com
+     *  
+     */
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpwd = "";
+    $dbname = "absensi_rampa1";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+ ?>
