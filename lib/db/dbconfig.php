27d102b8cb5e19e779a47e30be2ab57f3da4482b
Ameng <107167038+cameliasyarifa@users.noreply.github.com>
2022-08-04 16:39:55 +0700

    Update dbconfig.php
    Signed-off-by: Ameng <107167038+cameliasyarifa@users.noreply.github.com>

--- lib/db/dbconfig.php
+++ lib/db/dbconfig.php
@@ -1,1 +1,14 @@
+<?php 
+    /**
+     * 
+     *  Absensi Guru PAUD ABATASA
+     *  
+     */
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpwd = "";
+    $dbname = "absensi_guru";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+ ?>
 
